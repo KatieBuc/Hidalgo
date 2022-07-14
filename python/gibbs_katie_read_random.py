08de8edc4743548b68fdfb7033623d0f651a4287
Katie Buchhorn <katiebuchhorn@gmail.com>
2022-07-14 18:31:23 +0100

    unit tests pass with tears of joy

--- python/gibbs_katie_read_random.py
+++ python/gibbs_katie_read_random.py
@@ -166,207 +166,208 @@ class hidalgo():
 
         sampling = np.empty(shape=0) 
 
-        for it in range(Niter):
+        #for it in range(Niter):
+        it=0
+        #### SAMPLING d ###############################
+        for k in range(K):
+            stop = False
 
-            #### SAMPLING d ###############################
-            for k in range(K):
-                stop = False
+            while stop ==False:
 
-                while stop ==False:
+                #r1 = random.random()*200 # random sample for d[k]
+                #r2 = random.random() # random number for accepting
+                r1 = random_list.pop(0)
+                r2 = random_list.pop(0)
 
-                    #r1 = random.random()*200 # random sample for d[k]
-                    #r2 = random.random() # random number for accepting
+                rmax = (a1[k]-1)/b1[k]
 
-                    r1 = random_list.pop(0)
-                    r2 = random_list.pop(0)
+                if (a1[k]-1>0): 
+                    frac = np.exp(-b1[k]*(r1-rmax)-(a1[k]-1)*(np.log(rmax)-np.log(r1))) 
+                else:
+                    frac = np.exp(-b1[k]*r1)
 
-                    rmax = (a1[k]-1)/b1[k]
-
-                    if (a1[k]-1>0): 
-                        frac = np.exp(-b1[k]*(r1-rmax)-(a1[k]-1)*(np.log(rmax)-np.log(r1))) 
-                    else:
-                        frac = np.exp(-b1[k]*r1)
-
-                    if (frac>r2):
-                        stop=True
-                        if(it%sampling_rate==0 and it>= Niter*burn_in):
-                            sampling = np.append(sampling, r1)
-                        d[k]=r1
-
-
-            #### SAMPLING p ###############################
-            for k in range(K-1):
-                stop = False
-
-                while stop ==False:
-
-                    #r1 = random.random() # random sample for p[k]
-                    #r2 = random.random() # random number for accepting
-
-                    r1 = random_list.pop(0)
-                    r2 = random_list.pop(0)
-
-                    rmax = (c1[k]-1)/(c1[k]-1+c1[K-1]-1)
-                    frac = ((r1/rmax)**(c1[k]-1))*(((1-r1)/(1-rmax))**(c1[K-1]-1))
-
-                    if(frac>r2):
-                        stop=True
-                        r1=r1*(1.-pp+p[k])
-                        p[K-1]+=p[k]-r1
-                        pp-=p[k]-r1
-                        p[k]=r1
-                        if(it%sampling_rate==0 and it>= Niter*burn_in):
-                            sampling = np.append(sampling, r1)
-
-            if(it%sampling_rate==0 and it>= Niter*burn_in):
-                sampling = np.append(sampling, (1-pp))
-
-
-            #### SAMPLING zeta ###############################
-            stop=False
-            maxval=-100000
-            mx=0
-            l=0
-
-            if bool(use_Potts)==True and bool(estimate_zeta)==True:
-                for l in range(10):
-                    zeta1=0.5+0.05*l
-                    ZZ = np.empty((K,0))
-                    for k in range(K):
-                        ZZ =np.append(ZZ, Zpart(N_, NN[k], zeta1, q))
-                    h = 0
-                    for k in range(K):
-                        h=h+NN[k]*np.log(ZZ[k])
+                if (frac>r2):
+                    stop=True
+                    #if(it%sampling_rate==0 and it>= Niter*burn_in):
+                    sampling = np.append(sampling, r1)
                     
-                    val = (f1[0]-1)*np.log(zeta1)+(f1[1]-1)*np.log(1-zeta1)-h
+                    d[k]=r1
 
-                    if(val > maxval):
-                        maxval=val # found max val for below frac
-                        mx=zeta1
- 
-                while stop == False and bool(use_Potts)==True and bool(estimate_zeta)==True:
-                    #r1 = random.random() # random sample for zeta
-                    #r2 = random.random() # random number for accepting
 
-                    r1 = random_list.pop(0)
-                    r2 = random_list.pop(0)
+        #### SAMPLING p ###############################
+        for k in range(K-1):
+            stop = False
 
-                    ZZ = np.empty((K,0))
-                    for k in range(K):
-                        ZZ =np.append(ZZ, Zpart(N_, NN[k], r1, q))
-                    h = 0
-                    for k in range(K):
-                        h=h+NN[k]*np.log(ZZ[k])
-                    
-                    val = (f1[0]-1)*np.log(r1)+(f1[1]-1)*np.log(1-r1)-h
-                    frac = np.exp(val - maxval)
+            while stop ==False:
 
-                    if frac > r2:
-                        stop = True
-                        if it > 0: zeta = r1
+                #r1 = random.random() # random sample for p[k]
+                #r2 = random.random() # random number for accepting
 
-            #### SAMPLING Z ###############################
+                r1 = random_list.pop(0)
+                r2 = random_list.pop(0)
 
-            for i in range(N_):
+                rmax = (c1[k]-1)/(c1[k]-1+c1[K-1]-1)
+                frac = ((r1/rmax)**(c1[k]-1))*(((1-r1)/(1-rmax))**(c1[K-1]-1))
 
-                if fixed_Z == True: break
+                if(frac>r2):
+                    stop=True
+                    r1=r1*(1.-pp+p[k])
+                    p[K-1]+=p[k]-r1
+                    pp-=p[k]-r1
+                    p[k]=r1
+                    #if(it%sampling_rate==0 and it>= Niter*burn_in):
+                    sampling = np.append(sampling, r1)
 
-                if abs((zeta-1)<1E-5):
-                    if(it%sampling_rate==0 and it>= Niter*burn_in):
-                        sampling = np.append(sampling, Z[i])
-                        continue
+        #if(it%sampling_rate==0 and it>= Niter*burn_in):
+        sampling = np.append(sampling, (1-pp))
+
+
+        #### SAMPLING zeta ###############################
+        stop=False
+        maxval=-100000
+        mx=0
+        l=0
+
+        if bool(use_Potts)==True and bool(estimate_zeta)==True:
+            for l in range(10):
+                zeta1=0.5+0.05*l
+                ZZ = np.empty((K,0))
+                for k in range(K):
+                    ZZ =np.append(ZZ, Zpart(N_, NN[k], zeta1, q))
+                h = 0
+                for k in range(K):
+                    h=h+NN[k]*np.log(ZZ[k])
                 
-                stop = False
-                prob = np.empty(shape=K)
-                gg = np.empty(shape=K)
-                norm = 0
-                gmax = 0
+                val = (f1[0]-1)*np.log(zeta1)+(f1[1]-1)*np.log(1-zeta1)-h
 
-                for k1 in range(K):
-                    g = 0
-                    if use_Potts == True:
-                        n_in = 0
-                        for j in range(q):
-                            index = int(Iin[q*i+j])
-                            if Z[index]==k1: n_in=n_in +1.
-                        m_in = 0
-                        for j in range(int(Iout_count[i])):
-                            index = int(Iout[Iout_track[i] + j])
-                            if index > -1 and Z[index] == k1: m_in = m_in + 1.
+                if(val > maxval):
+                    maxval=val # found max val for below frac
+                    mx=zeta1
 
-                        g = (n_in+m_in)*np.log(zeta/(1-zeta))-np.log(Zpart(N_,NN[k1],zeta,q))
-                        g=g+np.log(Zpart(N_,NN[k1]-1,zeta,q)/Zpart(N_,NN[k1],zeta,q))*(NN[k1]-1)
+            while stop == False:
+                #r1 = random.random() # random sample for zeta
+                #r2 = random.random() # random number for accepting
 
-                    if g > gmax: gmax=g
-                    gg[k1]=g
+                r1 = random_list.pop(0)
+                r2 = random_list.pop(0)
 
-                for k1 in range(K):
-                    gg[k1]=np.exp(gg[k1]-gmax)
-
-                for k1 in range(K):
-                    prob[k1]=p[k1]*d[k1]*MU[i]**(-(d[k1]+1))*gg[k1]
-                    norm+=prob[k1]
-
-                for k1 in range(K):
-                    prob[k1]=prob[k1]/norm
+                ZZ = np.empty((K,0))
+                for k in range(K):
+                    ZZ =np.append(ZZ, Zpart(N_, NN[k], r1, q))
+                h = 0
+                for k in range(K):
+                    h=h+NN[k]*np.log(ZZ[k])
                 
-                while_loop_count = 0
-                while stop == False:
+                val = (f1[0]-1)*np.log(r1)+(f1[1]-1)*np.log(1-r1)-h
+                frac = np.exp(val - maxval)
 
-                    while_loop_count += 1
-                    #if while_loop_count > 10000: break
+                if frac > r2:
+                    stop = True
+                    if it > 0: zeta = r1
 
-                    #r1 = int(np.floor(random.random()*K))
-                    #r2 = random.random()
+        #### SAMPLING Z ###############################
 
-                    print(it, "r1", r1)
-                    r1 = int(random_list.pop(0))
-                    r2 = random_list.pop(0)
+        for i in range(N_):
 
-                    if prob[r1] > r2:
-                        stop = True
-                        if(it%sampling_rate==0 and it>= Niter*burn_in):
-                            sampling = np.append(sampling, r1)
-                        NN[Z[i]]-=1
-                        a1[Z[i]]-=1
-                        c1[Z[i]]-=1
-                        V[Z[i]]-=np.log(MU[i])
-                        b1[Z[i]]-=np.log(MU[i])
-                        Z[i]=r1
-                        NN[Z[i]]+=1
-                        a1[Z[i]]+=1
-                        c1[Z[i]]+=1
-                        V[Z[i]]+=np.log(MU[i])
-                        b1[Z[i]]+=np.log(MU[i])
+            if fixed_Z == True: break
 
-            #### updating prior on zeta ###############################
+            if abs(zeta-1)<1E-5:
+                #if(it%sampling_rate==0 and it>= Niter*burn_in):
+                sampling = np.append(sampling, Z[i])
+                continue            
 
-            N_in = 0
-            for i in range(N_):
-                k=Z[i]
-
-                for j in range(q):
-                    index=int(Iin[q*i+j])
-
-                    if(Z[index]==k): N_in=N_in+1.0
-
-
-            f1[0]=f[0]+N_in
-            f1[1]=f[1]+N_*q-N_in
-
-            #### likelihood ###############################
-            lik0=0
-            for i in range(N_):
-                lik0=lik0+np.log(p[Z[i]])+np.log(d[Z[i]])-(d[Z[i]]+1)*np.log(MU[i])
-            
-            lik1=lik0+np.log(zeta/(1-zeta))*N_in
+            stop = False
+            prob = np.empty(shape=K)
+            gg = np.empty(shape=K)
+            norm = 0
+            gmax = 0
 
             for k1 in range(K):
-                lik1=lik1-(NN[k1]*np.log(Zpart(N_, NN[k1], zeta, q)))
+                g = 0
+                if use_Potts == True:
+                    n_in = 0
+                    for j in range(q):
+                        index = int(Iin[q*i+j])
+                        if Z[index]==k1: n_in=n_in +1.
+                    m_in = 0
+                    for j in range(int(Iout_count[i])):
+                        index = int(Iout[Iout_track[i] + j])
+                        if index > -1 and Z[index] == k1: m_in = m_in + 1.
 
-            if(it%sampling_rate==0 and it>= Niter*burn_in):
-                sampling = np.append(sampling, lik0) 
-                sampling = np.append(sampling, lik1)
+                    g = (n_in+m_in)*np.log(zeta/(1-zeta))-np.log(Zpart(N_,NN[k1],zeta,q))
+                    g=g+np.log(Zpart(N_,NN[k1]-1,zeta,q)/Zpart(N_,NN[k1],zeta,q))*(NN[k1]-1)
+
+                if g > gmax: gmax=g
+                gg[k1]=g
+
+            for k1 in range(K):
+                gg[k1]=np.exp(gg[k1]-gmax)
+
+            for k1 in range(K):
+                prob[k1]=p[k1]*d[k1]*MU[i]**(-(d[k1]+1))*gg[k1]
+                norm+=prob[k1]
+
+            for k1 in range(K):
+                prob[k1]=prob[k1]/norm
+            
+            while_loop_count = 0
+            print("stop", stop)
+            while stop == False:
+
+                while_loop_count += 1
+                #if while_loop_count > 10000: break
+
+                #r1 = int(np.floor(random.random()*K))
+                #r2 = random.random()
+
+                print("r1", r1)
+                r1 = int(random_list.pop(0))
+                r2 = random_list.pop(0)
+
+                if prob[r1] > r2:
+                    stop = True
+                    #if(it%sampling_rate==0 and it>= Niter*burn_in):
+                    sampling = np.append(sampling, r1)
+                    NN[Z[i]]-=1
+                    a1[Z[i]]-=1
+                    c1[Z[i]]-=1
+                    V[Z[i]]-=np.log(MU[i])
+                    b1[Z[i]]-=np.log(MU[i])
+                    Z[i]=r1
+                    NN[Z[i]]+=1
+                    a1[Z[i]]+=1
+                    c1[Z[i]]+=1
+                    V[Z[i]]+=np.log(MU[i])
+                    b1[Z[i]]+=np.log(MU[i])
+
+        #### updating prior on zeta ###############################
+
+        N_in = 0
+        for i in range(N_):
+            k=Z[i]
+
+            for j in range(q):
+                index=int(Iin[q*i+j])
+
+                if(Z[index]==k): N_in=N_in+1.0
+
+
+        f1[0]=f[0]+N_in
+        f1[1]=f[1]+N_*q-N_in
+
+        #### likelihood ###############################
+        lik0=0
+        for i in range(N_):
+            lik0=lik0+np.log(p[Z[i]])+np.log(d[Z[i]])-(d[Z[i]]+1)*np.log(MU[i])
+        
+        lik1=lik0+np.log(zeta/(1-zeta))*N_in
+
+        for k1 in range(K):
+            lik1=lik1-(NN[k1]*np.log(Zpart(N_, NN[k1], zeta, q)))
+
+        #if(it%sampling_rate==0 and it>= Niter*burn_in):
+        sampling = np.append(sampling, lik0) 
+        sampling = np.append(sampling, lik1)
         
         return sampling
 
